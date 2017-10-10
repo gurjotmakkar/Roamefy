dea5a9c504cdf64d7ef5acf52de7db1261bf0968
Gurjot Singh Makkar <gurjotsingh427@gmail.com>
2017-10-10 14:46:02 -0400

    connected app to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,20 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseProvider } from './../providers/firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfAtP5YK9MsFgB7erifiiLX3XT4JouuY0",
+  authDomain: "roamefy-app.firebaseapp.com",
+  databaseURL: "https://roamefy-app.firebaseio.com",
+  projectId: "roamefy-app",
+  storageBucket: "roamefy-app.appspot.com",
+  messagingSenderId: "844616883402"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -17,6 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -28,7 +45,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
   ]
 })
 export class AppModule {}
